1c18453a67f971ecebef585f49fa1010b856f1e2
AnnaNikitchenko <nikitchenkoanna1@gmail.com>
2025-05-10 11:42:04 +0300

    Update is_balance.py

--- is_balance.py
+++ is_balance.py
@@ -4,14 +4,14 @@ def is_balanced(s):
         if symbol == '(':
             count += 1
         elif symbol == ')':
+            if count == 0:
+                return False
             count -= 1
-        if count < 0:
-            return False
     return count == 0
 
 
 print(is_balanced('()'))
 print(is_balanced('())'))
-print(is_balanced('(()'))
+print(is_balanced('(())'))
 print(is_balanced(')())'))
 print(is_balanced('))(('))
